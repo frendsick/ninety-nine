a288ce6f8bb95e19baed2fe8679255650ed2e81b
CyberPaddy <teemu@paddy.fi>
2023-05-15 23:50:41 +0300

    Put function inside object so it can be imported

--- scala/p09.scala
+++ scala/p09.scala
@@ -1,12 +1,15 @@
 import language.deprecated.symbolLiterals
-@main def p09 = {
-  val example = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
-  println(pack(example))
-}
 
-def pack(list: List[Any]): List[List[Any]] = {
-  if (list.isEmpty) List(List.empty)
-  val (packed, rest) = list.span { _ == list.head }
-  if (rest == Nil) return List(packed)
-  packed :: pack(rest)
+object p09 {
+  def main(args: Array[String]): Unit = {
+    val example = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    println(pack(example))
+  }
+
+  def pack(list: List[Any]): List[List[Any]] = {
+    if (list.isEmpty) List(List.empty)
+    val (packed, rest) = list.span { _ == list.head }
+    if (rest == Nil) return List(packed)
+    packed :: pack(rest)
+  }
 }
