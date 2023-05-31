10130a61b8f73bc677ac9f1ff2d78a3c08fe33eb
Teemu Pätsi <teemu@paddy.fi>
2023-06-01 00:10:49 +0300

    Put p20.removeAt inside an object to be used in P23

--- scala/p20.scala
+++ scala/p20.scala
@@ -1,7 +1,11 @@
 import language.deprecated.symbolLiterals
-@main def p20 = println(removeAt(1, List('a, 'b, 'c, 'd)))
+object p20 {
+  def main(args: Array[String]): Unit = println(
+    removeAt(1, List('a, 'b, 'c, 'd))
+  )
 
-def removeAt(k: Int, ls: List[Any]): Tuple2[List[Any], Option[Any]] = {
-  if (k < 0 || k >= ls.length) then(ls, None)
-  else (ls.take(k) ::: ls.drop(k + 1), Some(ls(k)))
+  def removeAt(k: Int, ls: List[Any]): Tuple2[List[Any], Option[Any]] = {
+    if (k < 0 || k >= ls.length) then(ls, None)
+    else (ls.take(k) ::: ls.drop(k + 1), Some(ls(k)))
+  }
 }
